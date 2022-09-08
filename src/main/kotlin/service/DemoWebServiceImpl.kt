2035a69bf71e9188619710717b3809a623da7a7a
krasnovss91 <krasnovstanislav91@gmail.com>
2022-09-08 16:37:21 +0400

    Service_2

--- src/main/kotlin/service/DemoWebServiceImpl.kt
+++ src/main/kotlin/service/DemoWebServiceImpl.kt
@@ -1,4 +1,19 @@
 package service
 
-class DemoWebServiceImpl {
+import javax.jws.WebService
+import kotlin.math.sqrt
+
+@WebService(endpointInterface = "client.DemoWebService")
+class DemoWebServiceImpl : DemoWebService {
+    override fun getResult(a: Int, b: Int, c: Int): Double {
+        val d = (b * b) - (4 * a * c)
+
+        return (b * (-1) + sqrt(d.toDouble())) / (2 * a)
+    }
+
+    override fun getResultNeg(a: Int, b: Int, c: Int): Double {
+        val d = (b * b) - (4 * a * c)
+
+        return (b * (-1) - sqrt(d.toDouble())) / (2 * a)
+    }
 }
